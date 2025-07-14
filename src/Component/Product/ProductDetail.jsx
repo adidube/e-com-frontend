1e4e2f36b15a8a58219f3ff6337ddc91395674ea
Aditya Dubey <ad334071@gmail.com>
2025-07-14 12:44:26 +0530

    changes in product details

--- src/Component/Product/ProductDetail.jsx
+++ src/Component/Product/ProductDetail.jsx
@@ -1,18 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import RelatedProducts from "./RelatedProducts";
+import AppContext from "../../Context/AppContext";
 
 function ProductDetail() {
+  const { addToCart } = useContext(AppContext);
   const [product, setProduct] = useState({});
   const { id } = useParams();
 
-  const url = "http://localhost:3000/api";
+  // const url = "http://localhost:3000/api";
+  const url = "https://e-com-backend-2-0j1n.onrender.com/api";
 
   useEffect(
     function () {
       async function fetchProduct() {
-        const api = await axios.get(`http://localhost:3000/api/product/${id}`, {
+        const api = await axios.get(`${url}/product/${id}`, {
           headers: {
             "Content-Type": "Application/json",
           },
@@ -29,36 +32,54 @@ function ProductDetail() {
 
   return (
     <>
-    
-   
       <div
         className="container text-center my-5"
         style={{
-            display: "flex",
-            justifyContent: "space-evenly",
-            alignItems: "center",
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignItems: "center",
         }}
       >
         <div className="left">
           <img
             src={product?.imgSrc}
             alt=""
-            style={{ width: "250px", height: "250px" ,borderRadius:'10px',border:"2px solid yellow"}}
+            style={{
+              width: "250px",
+              height: "250px",
+              borderRadius: "10px",
+              border: "2px solid yellow",
+            }}
           />
         </div>
         <div className="right">
           <h1>{product?.title}</h1>
           <p>{product?.description}</p>
-          <h1>{"₹"}{" "}{product?.price}</h1>
+          <h1>
+            {"₹"} {product?.price}
+          </h1>
           <div className="my-3">
-            <button className="btn btn-danger mx-3" style={{fontWeight:'bold'}}>Buy Now</button>
-            <button className="btn btn-warning" style={{fontWeight:'bold'}}>Add to Cart</button>
+            <button
+              className="btn btn-danger mx-3"
+              style={{ fontWeight: "bold" }}
+            >
+              Buy Now
+            </button>
+            <button className="btn btn-warning" style={{ fontWeight: "bold" }} 
+              onClick={()=>addToCart(
+                product._id,
+                product.title,
+                product.price,
+                1,
+                product.imgSrc
+              )}>
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
 
       <RelatedProducts category={product.category} />
-   
     </>
   );
 }
